36d2c50ae2d0a2fd467aacf1248d7b9edea28006
ChinoU <chinoukaegbu@gmail.com>
2024-07-22 18:46:58 +0400

    fetch game details

--- src/pages/GameDetailPage.tsx
+++ src/pages/GameDetailPage.tsx
@@ -1,7 +1,25 @@
+import { Heading, Spinner, Text } from "@chakra-ui/react";
+import { useQuery } from "@tanstack/react-query";
 import React from "react";
+import { useParams } from "react-router-dom";
+import useGame from "../hooks/useGame";
+import { Game } from "../hooks/useGames";
+import APIClient from "../services/api-client";
 
 const GameDetailPage = () => {
-  return <div>GameDetailPage</div>;
+  const { slug } = useParams();
+  const { data: game, isLoading, error } = useGame(slug!);
+
+  if (isLoading) return <Spinner />;
+
+  if (error || !game) throw error;
+
+  return (
+    <>
+      <Heading>{game.name}</Heading>
+      <Text>{game.description_raw}</Text>
+    </>
+  );
 };
 
 export default GameDetailPage;
